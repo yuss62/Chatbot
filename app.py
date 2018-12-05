6ea1d1be91d0644186b3c8a9ddd2d980cf6a46af
yuss62 <euscheyso62@gmail.com>
2018-12-05 08:58:56 +0800

    berlajar chatbot groupme

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def webhook():
 def reply(msg):
 	url = 'https://api.groupme.com/v3/bots/post'
 	data = {
-		'bot_id'		: bot_id,
+		'bot_id'		: dac6f9fe24fe7ffa8ab566d96d,
 		'text'			: msg
 	}
 	request = Request(url, urlencode(data).encode())
@@ -39,7 +39,7 @@ def reply(msg):
 def reply_with_image(msg, imgURL):
 	url = 'https://api.groupme.com/v3/bots/post'
 	data = {
-		'bot_id'		: bot_id,
+		'bot_id'		: dac6f9fe24fe7ffa8ab566d96d,
 		'text'			: msg,
 		'attachments'	: [{"type": "image", "url":imgURL}]
 	}
